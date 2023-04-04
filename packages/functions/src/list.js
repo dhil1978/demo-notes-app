1590beaa7e12c171ad67cbb25006965c321f1b18
Ana Mukoska <anamukoska@Anas-MacBook-Air.local>
2023-04-04 11:53:23 +0200

    Securing the API

--- packages/functions/src/list.js
+++ packages/functions/src/list.js
@@ -2,7 +2,7 @@ import { Table } from "sst/node/table";
 import handler from "@notes/core/handler";
 import dynamoDb from "@notes/core/dynamodb";
 
-export const main = handler(async () => {
+export const main = handler(async (event) => {
 	const params = {
 		TableName: Table.Notes.tableName,
 		// 'KeyConditionExpression' defines the condition for the query
